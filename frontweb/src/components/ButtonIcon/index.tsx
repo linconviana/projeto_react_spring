b855729049eb0c915b599d3949d5fc0e1f251873
linconviana <linconviana@gmail.com>
2021-10-14 17:41:04 -0300

    teste

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+  return <button type="button" className="btn btn-primary btn-icon">Inicie sua busca</button>;
+};
+
+export default ButtonIcon;
