262dfcc4cfe6367406ce8a2e5077f863cc5b63ba
linconviana <linconviana@gmail.com>
2021-10-15 14:35:22 -0300

    format price

---
+++ frontweb/src/util/formatter.ts
@@ -0,0 +1,8 @@
+export const FormatPrice = (price: number) => {
+
+    const params = {
+        minimumFractionDigits: 2, maximumFractionDigits: 2
+    };
+
+    return new Intl.NumberFormat('pt-BR', params).format(price);
+}
