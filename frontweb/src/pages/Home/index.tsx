55ed177151fb1ec3334c45cddfd718b8a6e17130
linconviana <linconviana@gmail.com>
2021-10-15 00:41:13 -0300

    responsive home style

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -2,21 +2,26 @@ import Navbar from "../../components/Navbar";
 import { ReactComponent as MainImage } from "assets/images/main-image.svg";
 import ButtonIcon from "../../components/ButtonIcon";
 
-import './styles.css';
+import "./styles.css";
 
 const Home = () => {
   return (
-    <> 
+    <>
       <Navbar />
       <div className="home-container">
         <div className="home-card">
           <div className="home-content-container">
-            <h1>Conheça o melhor catalogo de produtos</h1>
-            <p>Ajudaremos você a encontrar os melhores produtos disponiveis no mercado</p>
+            <div>
+              <h1>Conheça o melhor catalogo de produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponiveis no
+                mercado
+              </p>
+            </div>
             <ButtonIcon />
           </div>
           <div className="home-image-container">
-              <MainImage />
+            <MainImage />
           </div>
         </div>
       </div>
