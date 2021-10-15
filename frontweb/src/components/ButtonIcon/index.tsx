d274b4ab7592fc7a09b0ae48b75eff13b890775b
linconviana <linconviana@gmail.com>
2021-10-14 23:10:24 -0300

    responsive mobile

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,7 +1,11 @@
-import './styles.css';
+import "./styles.css";
 
 const ButtonIcon = () => {
-  return <button type="button" className="btn btn-primary btn-icon">Inicie sua busca</button>;
+  return (
+    <button type="button" className="btn btn-primary btn-icon">
+      <h6>Inicie sua busca</h6>
+    </button>
+  );
 };
 
 export default ButtonIcon;
