952e8e35eedd4932aa9d7de90d92c52d806bec37
linconviana <linconviana@gmail.com>
2021-10-18 10:50:44 -0300

    criação de rotas aninhadas, redirect e nesting

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Pagination from "components/Pagination";
 
 import "./styles.css";
+import { Switch, Route } from "react-router";
 
 const Admin = () => {
   return (
@@ -12,6 +13,17 @@ const Admin = () => {
         </div>
         <div className="col-md-9 admin-content">
           <h3>Menu itens</h3>
+          <Switch>
+            <Route path="/admin/products">
+              <h1>Product Crud</h1>
+            </Route>
+            <Route path="/admin/categories">
+              <h1>Category Crud</h1>
+            </Route>
+            <Route path="/admin/users">
+              <h1>User Crud</h1>
+            </Route>
+          </Switch>
           <Pagination />
         </div>
       </div>
