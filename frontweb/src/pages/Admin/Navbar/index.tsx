e5c3e258b468e8f3bf8e2e9d061366873d69ab4e
linconviana <linconviana@gmail.com>
2021-10-18 01:09:17 -0300

    criação componente navbar lateral admin

---
+++ frontweb/src/pages/Admin/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><p>Produtos</p></li>
+        <li><p>Categorias</p></li>
+        <li><p>Usuarios</p></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,6 +1,24 @@
-const Admin = () => {
 
-    return <h1>Admin</h1>
-}
+import Navbar from "./Navbar";
+import Pagination from "components/Pagination";
+
+import "./styles.css";
+
+const Admin = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-3 admin-navbar">
+        <h3>Navbar Lateral</h3>
+          <Navbar />
+        </div>
+        <div className="col-md-9 admin-content">
+          <h3>Menu itens</h3>
+          <Pagination />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Admin;
