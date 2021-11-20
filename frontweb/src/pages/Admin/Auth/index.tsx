8cc88ca542c8e357d6c8d02ccb03b6e7fcb6786b
linconviana <linconviana@gmail.com>
2021-11-20 02:40:30 -0300

    login card style

--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,27 +1,29 @@
 import Imagem from 'assets/images/auth-image.svg'
 import { Route, Switch } from 'react-router';
+import Login from './Login';
+
+import './styles.css'
 
 const Auth = () => {
     return(
-        <div className="row">
-            <div className="col-md-7 banner-container">
+        <div className="auth-container">
+            <div className="auth-banner-container">
                 <h1>Divulgue seus produto no DS Catalog</h1>
                 <p>Faça parte do nosso catalogo de divulgação e aumente a venda dos seus produtos.</p>
                 <img src={Imagem} alt="imagem"/>
             </div>
-            <div className="col-md-5 auth-form-container">
-                <h1>Login</h1>
-                    <Switch>
-                        <Route path="/admin/auth/login">
-                            <h1>Card Login</h1>
-                        </Route>
-                        <Route path="/admin/auth/signup">
-                            <h1>Card Signup</h1>
-                        </Route>
-                        <Route path="/admin/auth/recover">
-                            <h1>Card Recover</h1>
-                        </Route>
-                    </Switch>
+            <div className="auth-form-container">
+                <Switch>
+                    <Route path="/admin/auth/login">
+                        <Login />
+                    </Route>
+                    <Route path="/admin/auth/signup">
+                        <h1>Card Signup</h1>
+                    </Route>
+                    <Route path="/admin/auth/recover">
+                        <h1>Card Recover</h1>
+                    </Route>
+                </Switch>
             </div>
         </div>
     )
