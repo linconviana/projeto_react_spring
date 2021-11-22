b08e697a2bbb7586617fd17393d8dc689e7e5955
linconviana <linconviana@gmail.com>
2021-11-21 23:28:14 -0300

    add react-hook-form, falta corrigir erro

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,30 +1,44 @@
 import ButtonIcon from 'components/ButtonIcon';
+//import { useForm } from 'react-hook-form';
 import {Link} from 'react-router-dom';
-import { useForm } from "react-hook-form";
+import { requestBackendLogin } from 'util/requests';
 
 import './styles.css';
 
-type FormData = {
+/*type FormData = {
     username: string,
     password: string
-}
+}*/
 
 const Login = () => {
 
-    const { register, handleSubmit } = useForm<FormData>();
-    const onSubmit = (formData: FormData) => {
-        console.log(formData);
+    //const { register, handleSubmit } = useForm<FormData>();
+    //const onSubmit = (formData: FormData) => {
+    const handleSubmit = (event: any) => {
+
+        requestBackendLogin({username:'maria@gmail.com', password:'123456'})
+        .then(response => {
+            debugger
+            console.log('Sucesso', response);
+        })
+        .catch(error => {
+            debugger
+            console.log('Erro', error.message);
+        })
     }
 
+    // <form onSubmit={handleSubmit(onSubmit)}></form>
+    //<input {...register("username")} type="text" className="form-control base-input" placeholder="Email" name="username" />
+    //<input {...register("password")} type="password" className="form-control base-input" placeholder="Password" name="password" />
     return(
         <div className="base-card login-card">
             <h1>Login</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-4">
-                    <input {...register("username")} type="text" className="form-control base-input" placeholder="Email" name="username" />
+                    <input  type="text" className="form-control base-input" placeholder="Email" name="username" />
                 </div>
                 <div className="mb-2">
-                    <input {...register("password")} type="password" className="form-control base-input" placeholder="Password" name="password" />
+                    <input type="password" className="form-control base-input" placeholder="Password" name="password" />
                 </div>
                 <Link to="auth/recover" className="login-link-recover">Esqueci a senha</Link>
                 <div className="login-submit">
