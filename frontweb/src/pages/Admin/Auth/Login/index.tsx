e4825310ca7a8f8dc12404c4a85d14d053353221
linconviana <linconviana@gmail.com>
2022-01-31 23:27:21 -0300

    validação de formulario de login com hook-form

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -14,7 +14,7 @@ type FormData = {
 const Login = () => {
 
     const [hasError, setHasError] = useState(false);
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
     const onSubmit = (formData: FormData) => {
 
         requestBackendLogin(formData)
@@ -42,10 +42,20 @@ const Login = () => {
             }
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
-                    <input {...register("username")} type="text" className="form-control base-input" placeholder="Email" name="username" />
+                    <input {...register("username", {
+                        required: 'Campo Obrigatorio', 
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: 'Email inválido!'
+                        }
+                    })} type="text" className="form-control base-input" placeholder="Email" name="username" />
+                    <div className="invalid-feedback d-block">{errors.username?.message}</div>
                 </div>
                 <div className="mb-2">
-                    <input {...register("password")} type="password" className="form-control base-input" placeholder="Password" name="password" />
+                    <input {...register("password", {
+                        required: 'Campo Obrigatorio'
+                    })} type="password" className="form-control base-input" placeholder="Password" name="password" />
+                    <div className="invalid-feedback d-block">{errors.password?.message}</div>
                 </div>
                 <Link to="auth/recover" className="login-link-recover">Esqueci a senha</Link>
                 <div className="login-submit">
