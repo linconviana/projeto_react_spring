b34324344dc271bde1723404dbf4ed017e48f887
Lincon Santos Viana Barbosa <linconviana@gmail.com>
2022-12-16 09:04:24 -0300

    ajuste tamanho dropzone

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -2,7 +2,7 @@ package com.devsuperior.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/// :: https://github.com/devsuperior/dscatalog-resources/tree/master/backend
 @SpringBootApplication
 public class DscatalogApplication {
 
